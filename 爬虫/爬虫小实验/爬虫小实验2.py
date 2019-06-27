2943c63dee9b8820eaf21f1773b033ecb46670d0
1743294154 <1743294154@qq.com>
2019-06-27 22:51:57 +0800

    Default Changelist

--- 爬虫/爬虫小实验/爬虫小实验2.py
+++ 爬虫/爬虫小实验/爬虫小实验2.py
@@ -1,4 +1,5 @@
 #抓一个python官网的网页
 import urllib.request
 response = urllib.request.urlopen('https://www.python.org')
-print(response.read().decode('utf-8'))
+#print(response.read().decode('utf-8'))
+print(type(response))#利用type（）方法输出响应的类型。
